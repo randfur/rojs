320b386ec23bb948bc5257838830f02d0f701cc3
Alan Cutter <alancutter@chromium.org>
2023-04-07 12:40:34 +1000

    remove

--- main.js
+++ main.js
@@ -14,14 +14,198 @@
  * limitations under the License.
  */
 
-import {nonReactiveExample} from './non-reactive.js';
-import {reactiveExample} from './reactive.js';
+import {
+  array,
+  coinFlip,
+  join,
+  random,
+  range,
+  sleep,
+  enumerate,
+} from './utils.js';
+import {
+  createObservableJsonProxy,
+  mutate,
+  printObservation,
+  read,
+  write,
+} from './observable-json.js';
+import {
+  br,
+  div,
+  flexRow,
+  htmlRead,
+  htmlIf,
+  htmlSwitch,
+  htmlMapRead,
+  render,
+} from './render.js';
 
 async function main() {
-  await new Promise(resolve => document.addEventListener('DOMContentLoaded', resolve));
+  render(
+    document.body,
+    flexRow(
+      div(resizingSiblings()),
+      div(dogCow()),
+      div(dogCat()),
+    ),
+  );
+}
 
-  // document.body.append(nonReactiveExample());
-  reactiveExample();
+function resizingSiblings() {
+  const colours = ['red', 'yellow', 'lime', 'blue'];
+
+  const modelProxy = createObservableJsonProxy(
+    Object.fromEntries(
+      colours.map(colour => [colour, 3])
+    )
+  );
+
+  for (const [i, colour] of enumerate(colours)) {
+    setInterval(() => {
+      write(modelProxy[colour], Math.round(random(3)));
+    }, 1000 + i * 100);
+  }
+
+  const colourBlockStyle = {
+    margin: '1px',
+    width: '20px',
+    height: '20px',
+    textAlign: 'center',
+    color: 'black',
+  };
+
+  return {
+    style: { width: '200px' },
+    children: [
+      join([
+        'Resizing siblings',
+        'Values: ',
+        ...colours.map(colour => array`- ${colour}: ${modelProxy[colour]}`),
+      ], br),
+      colours.map(
+        colour => htmlRead(
+          modelProxy[colour],
+          count => range(count).map(
+            i => ({
+              style: {
+                ...colourBlockStyle,
+                backgroundColor: colour,
+              },
+              textContent: i,
+            })
+          )
+        )
+      ),
+    ],
+  };
+}
+
+function dogCow() {
+  let modelProxy = createObservableJsonProxy({
+    mode: 'dog',
+    dog: {
+      emoji: '🐶',
+      value: 30,
+    },
+    cow: {
+      emoji: '🐄',
+      value: 0,
+    },
+  });
+
+  setInterval(() => {
+    write(modelProxy.mode, coinFlip() ? 'dog' : 'cow');
+  }, 3100);
+
+  setInterval(() => {
+    write(modelProxy.dog.value, 20 + random(20));
+  }, 700);
+
+  setInterval(() => {
+    write(modelProxy.cow.value, random(100));
+  }, 600);
+
+  return [
+    'Dog cow',
+    {
+      style: () => {
+        const mode = read(modelProxy.mode);
+        const valueProxy = modelProxy[mode].value;
+        const result = {
+          height: '40px',
+        };
+        if (mode === 'dog') {
+          result.fontSize = () => `${read(valueProxy)}px`;
+        } else {
+          result.fontSize = '20px';
+          result.marginLeft = () => `${read(valueProxy)}px`;
+        }
+        return result;
+      },
+      textContent: () => read(modelProxy[read(modelProxy.mode)].emoji),
+    },
+    {
+      tag: 'pre',
+      onCreate(element) {
+        setInterval(() => {
+          element.textContent = printObservation(modelProxy);
+        }, 100);
+      }
+    },
+  ];
+}
+
+function dogCat() {
+  const modelProxy = createObservableJsonProxy({
+    mode: 'dog',
+    dogData: {
+      barkbark: true,
+      walkies: true,
+    },
+    catData: {
+      meowmeow: true,
+      mice: [],
+    },
+  });
+
+  setInterval(() => {
+    write(modelProxy.mode, coinFlip() ? 'dog' : 'cat');
+  }, 2000);
+
+  setInterval(() => {
+    write(modelProxy.dogData.walkies, Math.round(random(4)));
+  }, 700);
+  setInterval(() => {
+    write(modelProxy.dogData.barkbark, coinFlip());
+  }, 500);
+
+  setInterval(() => {
+    write(modelProxy.catData.meowmeow, coinFlip());
+  }, 800);
+  setInterval(() => {
+    write(modelProxy.catData.mice, range(Math.round(random(4))).map(i => coinFlip()));
+  }, 600);
+
+  return {
+    style: {
+      fontSize: '60px',
+    },
+    children: htmlSwitch(modelProxy.mode, {
+      dog: [
+        '🐶',
+        htmlIf(modelProxy.dogData.barkbark, '💬Bark bark!'),
+        br,
+        htmlRead(modelProxy.dogData.walkies, walkies => '🐾'.repeat(walkies)),
+      ],
+      cat: [
+        '🐱',
+        htmlIf(modelProxy.catData.meowmeow, '💬Meow meow.'),
+        br,
+        htmlMapRead(modelProxy.catData.mice, mouse => mouse ? '🐀' : '🐁'),
+      ],
+    }),
+  };
 }
 
 main();
